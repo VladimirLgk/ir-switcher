c061792ca45c204ce00a0f4c5437b9d619ad3d8e
Vladimir Legky <vlegky@volgasoft.com>
2014-01-07 17:01:37 +0400

    remove useless files

--- receiver.X/build/default/debug/receiver.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/receiver.o:  \
-	D:/Vladimir\ Legky/development/irpair/receiver.X/receiver.asm  \
-
--- receiver.X/build/default/production/receiver.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/receiver.o:  \
-	D:/Vladimir\ Legky/development/irpair/receiver.X/receiver.asm  \
-
