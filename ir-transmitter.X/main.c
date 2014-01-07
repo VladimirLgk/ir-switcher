97be46b74339edcd37afe69afd1ed7794b415e24
Vladimir Legky <vlegky@volgasoft.com>
2014-01-08 01:22:58 +0400

    initial commit

---
+++ ir-transmitter.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Vladimir Legky
+ *
+ * Created on 08 January 2014, 01:20
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
